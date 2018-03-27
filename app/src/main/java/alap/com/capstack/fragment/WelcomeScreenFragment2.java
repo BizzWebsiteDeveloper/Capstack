5fd60f8211951fb0f0642ce5dfc121bcfa620e20
jay <jeckygauswami123@gmail.com>
2018-03-27 13:03:23 +0530

    WecomeScreen1

--- app/src/main/java/alap/com/capstack/fragment/WelcomeScreenFragment2.java
+++ app/src/main/java/alap/com/capstack/fragment/WelcomeScreenFragment2.java
@@ -1,6 +1,7 @@
 package alap.com.capstack.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import alap.com.capstack.R;
+import alap.com.capstack.activity.InformationWindow2;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +30,14 @@ public class WelcomeScreenFragment2 extends Fragment {
                              Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_welcome_screen_fragment2, container, false);
         btn_login=(Button)v.findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getContext(), InformationWindow2.class);
+                startActivity(i);
+
+            }
+        });
         return v;
     }
 
